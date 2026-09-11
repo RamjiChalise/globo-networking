d1ae5c7f101dd57b3af984046d69cfcbd6d464ce
ramji.chalise2@knox.vic.gov.au <ramji.chalise2@knox.vic.gov.au>
2026-09-11 13:21:36 +1000

    Add third subnet and tags

--- variables.tf
+++ variables.tf
@@ -20,6 +20,10 @@ variable "environment" {
   default     = "development"
 }
 
+variable "billing_code" {
+  type = string
+  description = "Required: Billing Code for the environment"
+}
 variable "cidr_block" {
   type        = string
   description = "(Optional) The CIDR block for the VPC. Default:10.42.0.0/16"
